19c430209efd02ae8757f87775834a0b528cedba
Pentaminer <150195480+Pentaminer@users.noreply.github.com>
2026-05-10 15:11:32 +0300

    Create RoseGoldMaterial.java

---
+++ src/main/java/com/pentaminer/scraptech/material/RoseGoldMaterial.java
@@ -0,0 +1,44 @@
+package com.pentaminer.scraptech.material;
+
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.item.Items;
+
+public class RoseGoldMaterial implements ToolMaterial {
+
+    // 1. Прочность (сколько ударов/добычи выдержит)
+    @Override
+    public int getDurability() {
+        return 500;
+    }
+
+    // 2. Скорость добычи блоков
+    @Override
+    public float getMiningSpeedMultiplier() {
+        return 6.0f;
+    }
+
+    // 3. Урон по блокам (не по мобам)
+    @Override
+    public float getAttackDamage() {
+        return 1.5f;
+    }
+
+    // 4. Уровень добычи (0 дерево, 1 камень, 2 железо, 3 алмаз)
+    @Override
+    public int getMiningLevel() {
+        return 3;
+    }
+
+    // 5. Зачаровываемость
+    @Override
+    public int getEnchantability() {
+        return 20;
+    }
+
+    // 6. Ремонт (чем чинится инструмент)
+    @Override
+    public Ingredient getRepairIngredient() {
+        return Ingredient.ofItems(ScrapTech.ROSE_GOLD_INGOT);
+    }
+}
