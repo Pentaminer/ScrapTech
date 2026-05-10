5450fd420999a51dca3647a052938e7744837dea
Pentaminer <150195480+Pentaminer@users.noreply.github.com>
2026-05-10 13:22:58 +0300

    Update ScrapTech.java

--- src/main/java/com/pentaminer/scraptech/ScrapTech.java
+++ src/main/java/com/pentaminer/scraptech/ScrapTech.java
@@ -3,6 +3,7 @@ package com.pentaminer.scraptech;
 import net.fabricmc.api.ModInitializer;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.Sound;
 import net.minecraft.block.Material;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.item.BlockItem;
@@ -53,6 +54,17 @@ public class ScrapTech implements ModInitializer {
             Registry.ITEM,
             new Identifier(MOD_ID, "rough_fabric_block"),
             new BlockItem(ROUGH_FABRIC_BLOCK, new Item.Settings().group(SCRAPTECH_GROUP))
+    );
+    public static final Block ROSE_GOLD_BLOCK = Registry.register(
+        Registry.BLOCK,
+        new Identifier(MOD_ID, "rose_gold_block"),
+        new Block(AbstractBlock.Settings.of(Material.METAL).strength(1.0f).sounds(BlockSoundGroup.METAL))
+    );
+
+    public static final Item ROSE_GOLD_BLOCK_ITEM = Registry.register(
+        Registry.ITEM,
+        new Identifier(MOD_ID, "rose_gold_block"),
+        new BlockItem(BLOCK_NAME, new Item.Settings().group(SCRAPTECH_GROUP))
     );
 
     // =========================
